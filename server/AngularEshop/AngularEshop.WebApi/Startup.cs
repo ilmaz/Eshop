db4b5c0844382bc0a7cc1f388d39207cf93bbc9c
ilmaz <ilmaz1987@hotmail.com>
2020-04-08 13:22:25 +0430

    Add backend's Infrastructure

---
+++ server/AngularEshop/AngularEshop.WebApi/Startup.cs
@@ -0,0 +1,53 @@
+using System.IO;
+using AngularEshop.Core.Utilities.Extensions.Connection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AngularEshop.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IHostEnvironment hostEnvironment)
+        {
+            Configuration = configuration;
+            HostEnvironment = hostEnvironment;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IHostEnvironment HostEnvironment { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IConfiguration>(
+              new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                  .AddJsonFile($"appsettings.json")
+                  .Build()
+          );
+
+            services.AddApplicationDbContext(Configuration);
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
